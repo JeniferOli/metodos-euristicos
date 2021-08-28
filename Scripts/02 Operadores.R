7372087cd3c6096035cd607c60b57728fbebfb96
JeniferOli <89302790+JeniferOli@users.noreply.github.com>
2021-08-28 09:14:04 -0300

    Script-02

---
+++ Scripts/02 Operadores.R
@@ -0,0 +1,52 @@
+# Operadores Básicos, Relacionais e Lógicos em R
+
+# Operadores Básicos
+
+# Soma
+5 + 5
+
+# Subtração
+7 - 3
+
+# Multiplicação
+5 * 3
+
+# Divisão
+5 / 3
+
+# Potência
+3 ^ 2
+3 ** 2
+
+# Módulo
+16 %% 3
+
+# Operadores Relacionais
+x <- 7
+y <- 5
+x + y
+x <- 3
+x
+
+# Operadores
+x > 7
+x < 8 
+x <= 8
+x >- 8
+x == 8
+x != 8
+
+# Operadores Lógicos
+
+# And
+(x==8) & (x==6)
+(x==7) & (x>=5)
+(x==8) & (x==7)
+
+# Or
+(x==8) | (x>5)
+(x==8) | (x>=5)
+
+# Not
+x > 8
+print(!x >8)
